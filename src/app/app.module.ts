ce649ffc3aec87dd1932b8f0c868205d9b54617e
unknown <zerontecnologia1@gmail.com>
2018-10-06 23:54:06 -0400

    generacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { GaleryComponent } from './pages/galery/galery.component';
+import { DesboComponent } from './pages/desbo/desbo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    GaleryComponent,
+    DesboComponent
   ],
   imports: [
     BrowserModule
